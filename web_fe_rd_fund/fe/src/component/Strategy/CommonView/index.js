d84ab25f84ef1a343f9270edab29050a86356ea2
chenyuanbao <chenyuanbao@baidu.com>
2021-11-10 20:29:33 +0800

    常用视图-初始化

--- web_fe_rd_fund/fe/src/component/Strategy/CommonView/index.js
+++ web_fe_rd_fund/fe/src/component/Strategy/CommonView/index.js
@@ -1,40 +1,75 @@
 import React, {Component} from "react";
 import MyH1 from '../../Common/H1';
+import {
+    Button,
+    Input,
+} from 'antd';
+import {myError} from '../../Common/Message';
 
 import './index.less';
 
-class Bond extends Component {
+
+const { TextArea } = Input;
+
+class CommonView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // 策略列表。默认有1条
-            strategyList: [
-                [{probability: 0, rate: 0}],
-            ],
+            strategyListStr: '100, 10 \n 50, 20 ; 50, -15'
         };
     }
 
+    changeStrategyListStr = (e) => {
+        const {target: {value}} = e;
+        this.$set({
+            strategyListStr: value
+        });
+    }
+
+    submit = () => {
+        const {strategyListStr} = this.state;
+        const message = this.checkStrategyListStr(strategyListStr);
+        if (!message) {
+            myError(message);
+        }
+
+        // 校验通过，继续处理
+
+    }
+
+    checkStrategyListStr = (strategyListStr = '') => {
+        const strategyList = strategyListStr.split('\n').map(strategy => {
+            let itemList = [];
+            strategy.split(';').map(item => {
+                let [count, rate] = item.split(',');
+                count = parseFloat(count);
+                rate = parseFloat(rate);
+                itemList.push({count, rate});
+            })
+            return itemList;
+        });
+        debugger
+    }
+
     render() {
-        const {strategyList} = this.state;
+        const {strategyListStr} = this.state;
 
         return (
             <div className="common-view">
-                <MyH1 title="策略生成" />
                 <div className="strategy-list">
-                    {
-                        strategyList.map((strategy, index) => {
-                            return <div>策略{index + 1}</div>
-                            
-                            // strategy.map(item => {
-                            //     const {probability, rate} = item;
-                            //     return 
-                            // })
-                        })
-                    }
+                    <TextArea rows={5} placeholder="请输入策略列表（ 格式形如 100, 10 \n 50, 20 ; 50, -15 ）"
+                        value={strategyListStr}
+                        allowClear
+                        onChange={this.changeStrategyListStr}>
+                    </TextArea>
+                    <Button type="primary" onClick={this.submit}>确定</Button>
+                </div>
+                <div className="strategy-view">
+                    
                 </div>
             </div>
         )
     }
 }
 
-export default Bond;
+export default CommonView;
